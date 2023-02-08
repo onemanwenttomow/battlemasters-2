c9ab952e8afacc4ccd8cd5eb5dc6024b5bfbfb92
Peter Anderson <pete.anderson@gmail.com>
2023-02-08 15:16:16 +0100

    --add: active units indicated

--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -2,13 +2,14 @@ import { create } from "zustand";
 
 import units from "lib/units.json";
 import playingCards from "lib/cards.json";
-import { Unit, PlayingCards, Card } from "types";
+import { Unit, PlayingCards } from "types";
 import { shuffle } from "lib/utils";
 
 interface GameState {
   units: Unit[];
   playingCards: PlayingCards;
   playedCards: PlayingCards;
+  gameStarted: boolean;
   getUnitByCoords: (x: number, y: number) => Unit | undefined;
   tileHasUnit: (x: number, y: number) => boolean;
   shufflePlayingCards: () => void;
@@ -16,25 +17,32 @@ interface GameState {
 }
 
 const useGameStore = create<GameState>((set, get) => ({
-  units,
-  playingCards,
-  playedCards: [],
-  shufflePlayingCards: () =>
-    set((state) => ({ playingCards: shuffle(state.playingCards) })),
-  drawNextCard: () =>
-    set((state) => {
-      // if all cards have been played shuffle a new deck
-      const newPlayingCards = state.playingCards.length === 1 ? shuffle(playingCards) : [...state.playingCards];
-      const playedCards = [
-        ...state.playedCards,
-        newPlayingCards.shift()
-      ] as PlayingCards;
-      return { playingCards: newPlayingCards, playedCards }
-    }),
-  getUnitByCoords: (x: number, y: number) =>
-    get().units.find((unit) => unit.x === x && unit.y === y),
-  tileHasUnit: (x: number, y: number) =>
-    get().units.some((unit) => unit.x === x && unit.y === y),
+    units,
+    playingCards,
+    playedCards: [],
+    gameStarted: false,
+    shufflePlayingCards: () =>
+        set((state) => ({
+            playingCards: shuffle(state.playingCards),
+            gameStarted: true
+        })),
+    drawNextCard: () =>
+        set((state) => {
+            // if all cards have been played shuffle a new
+            const newPlayingCards =
+                state.playingCards.length === 1
+                    ? shuffle(playingCards)
+                    : [...state.playingCards];
+            const playedCards = [
+                ...state.playedCards,
+                newPlayingCards.shift()
+            ] as PlayingCards;
+            return { playingCards: newPlayingCards, playedCards };
+        }),
+    getUnitByCoords: (x: number, y: number) =>
+        get().units.find((unit) => unit.x === x && unit.y === y),
+    tileHasUnit: (x: number, y: number) =>
+        get().units.some((unit) => unit.x === x && unit.y === y)
 }));
 
 export default useGameStore;
--- src/app/Unit.tsx
+++ src/app/Unit.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Unit } from "../../types";
 import useStore from "../../hooks/useStore";
 
 interface Props {
@@ -6,18 +7,22 @@ interface Props {
   y: number;
 }
 
-export default function Unit({ x, y }: Props) {
-  const getUnitByCoords = useStore((state) => state.getUnitByCoords);
-  const unit = getUnitByCoords(x, y);
+export default function UnitImage({ x, y }: Props) {
+  const { getUnitByCoords, gameStarted } = useStore((state) => state);
+  const activeCard = useStore((store) => store.playingCards[0]);
+
+  const unit = getUnitByCoords(x, y) as Unit;
   function handleClick() {
     console.log(unit);
   }
 
+
   return (
     <Image
-      src={unit?.src || "/error.png"}
-      alt={unit?.alt || "/error.png"}
-      className="unit-shadow mx-auto top-5 relative border border-1 border-gray-700 "
+      src={unit.src || "/error.png"}
+      alt={unit.alt || "/error.png"}
+      className={`unit-shadow mx-auto top-5 relative border border-1 border-gray-700 
+          ${gameStarted && activeCard?.ids.includes(unit.id) && 'animate-bounce'}`}
       width="32"
       height="32"
       onClick={handleClick}
