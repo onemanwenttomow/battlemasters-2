41bde1e0a5a7f18746c938476cd37a5fa7ea87bc
Peter Anderson <pete.anderson@gmail.com>
2023-02-25 17:54:16 +0100

    --fix: potential no tower or ditch temp hack

--- lib/utils.ts
+++ lib/utils.ts
@@ -223,6 +223,9 @@ export function getCurrentOgreCard(acc: null | OgreCard, card: OgreCard) {
 
 function getTowerOffset(board: Array<Array<Tile>>) {
   const towerRow = board.find((row) => row.includes("to")) as Tile[];
+  if (!towerRow) {
+    return [999, 999];
+  }
   const towerY = board.indexOf(towerRow);
   const towerX = towerRow.indexOf("to");
   return [towerX, towerY];
@@ -235,6 +238,9 @@ export function isTowerTile(offset: Offset, board: Array<Array<Tile>>) {
 
 function getDitchOffset(board: Array<Array<Tile>>) {
   const ditchRow = board.find((row) => row.includes("di")) as Tile[];
+  if (!ditchRow) {
+    return [999, 999];
+  }
   const ditchY = board.indexOf(ditchRow);
   const ditchX = ditchRow.indexOf("di");
   return [ditchX, ditchY];
