e50b921ced9a134f61fe4a7a19de7f65469c90fc
Peter Anderson <pete.anderson@gmail.com>
2023-02-28 12:08:39 +0100

    --add: units not on board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import CurrentTurn from "./components/CurrentTurn";
 import useGameStore from "hooks/useStore";
 import OgreCards from "./components/OgreCards";
 import CampaignSelection from "./components/CampaignSelection";
+import Image from "next/image";
 
 export default function Home() {
   const { battleInProgress, units, board } = useGameStore((store) => store);
@@ -13,6 +14,12 @@ export default function Home() {
   const allUnitsOnBoard = units.every((unit) => unit.x && unit.y);
   // display units on not on board
   const unitsNotOnBoard = units.filter((unit) => !unit.x && !unit.y);
+  const chaosUnitsNotOnBoard = unitsNotOnBoard.filter(
+    (unit) => unit.army === "Chaos"
+  );
+  const imperialUnitsNotOnBoard = unitsNotOnBoard.filter(
+    (unit) => unit.army === "Imperial"
+  );
 
   return (
     <main className="p-4 grid grid-cols-[_1fr_1fr]">
@@ -23,6 +30,19 @@ export default function Home() {
         <CurrentTurn />
         <OgreCards />
         <h2>All units on board: {allUnitsOnBoard.toString()}</h2>
+        <h3>Chaos units not on board</h3>
+        <div className="flex flex-wrap">
+          {chaosUnitsNotOnBoard.map((unit) => (
+            <Image
+              key={unit.id}
+              src={unit.src}
+              alt={unit.id}
+              width="32"
+              height="32"
+              className="cursor-pointer"
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
