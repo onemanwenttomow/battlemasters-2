ea5edf3b1866ca651ea8c27e5533d0d7f62256d5
Peter Anderson <pete.anderson@gmail.com>
2023-03-26 14:37:14 +0200

    --fix: remove youtube to test link issue in production

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-import YouTube from "react-youtube";
+// import YouTube from "react-youtube";
 
 export default function About() {
   return (
     <>
       <div className="bg-white bg-opacity-70" style={{ height: "635px" }}>
         <h2>About</h2>
-        <YouTube videoId="SfFl9Zvm5Vk" />
+        {/* <YouTube videoId="SfFl9Zvm5Vk" /> */}
       </div>
       <div
         className="relative bg-white bg-opacity-0  row-start-1 row-end-2 col-start-2"
